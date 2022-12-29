b56b912f6e345e385c89e044edac489d8979aac0
nhannht <nhanclassroom@gmail.com>
2022-12-29 14:20:48 +0700

    back up

---
+++ craco.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    webpack: {
+        configure: (webpackConfig, {env, paths}) => {
+            return {
+                ...webpackConfig,
+                entry: {
+                    main: [env === 'development' &&
+                    require.resolve('react-dev-utils/webpackHotDevClient'), paths.appIndexJs].filter(Boolean),
+                    content: './src/content.js',
+                },
+                output: {
+                    ...webpackConfig.output,
+                    filename: 'static/js/[name].js',
+                },
+                optimization: {
+                    ...webpackConfig.optimization,
+                    runtimeChunk: false,
+                },
+                resolve: {
+                    fallback: {
+                        "url": false
+                    }
+                }
+
+            }
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        modules: true,
+                        localIdentName: '[local]'
+                    },
+                },
+            },
+        ],
+    },
+
+}
